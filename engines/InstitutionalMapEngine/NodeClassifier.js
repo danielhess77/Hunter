c2d589ce9ec3dec96cc563ac5d4685966f36c766
danielhess77 <daniel.hess7@gmail.com>
2026-07-07 14:35:26 +0000

    Create Node Classifier

---
+++ engines/InstitutionalMapEngine/NodeClassifier.js
@@ -0,0 +1,21 @@
+/**
+ * Node Classifier
+ * Version 1.0
+ *
+ * Determines the institutional role of a node.
+ */
+
+class NodeClassifier {
+
+    classify(node) {
+
+        return {
+            type: "Unknown",
+            magnitude: 0
+        };
+
+    }
+
+}
+
+export default NodeClassifier;
