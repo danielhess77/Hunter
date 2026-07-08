b2f26c56948480c52b50dabe2cca0e841daf1806
danielhess77 <daniel.hess7@gmail.com>
2026-07-08 01:07:00 +0000

    Create Hunter integration test

---
+++ tests/DataConnectorTest.js
@@ -0,0 +1,16 @@
+/**
+ * Hunter Integration Test
+ *
+ * Verifies that the Hunter Data Connector
+ * can populate HunterMarketState from a
+ * real Skylit export.
+ */
+
+import HunterDataConnector from "../connectors/HunterDataConnector/HunterDataConnector.js";
+import HunterMarketState from "../runtime/HunterMarketState.js";
+
+const connector = new HunterDataConnector();
+
+const marketState = new HunterMarketState();
+
+console.log("Hunter Integration Test Ready");
